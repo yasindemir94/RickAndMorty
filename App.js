18b78cb405df41facee015b6da97d9311019e5ea
yasindemir94 <yasin.demir94@outlook.com>
2021-03-24 16:17:41 +0300

    navigation updates

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, Text, StatusBar } from 'react-native'
 
 import Home from './src/views/Home'
 import EpisodeDetail from './src/views/EpisodeDetail';
+import Characters from './src/views/Characters';
+import CharacterDetail from './src/views/CharacterDetail';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -17,6 +19,8 @@ const App = () => {
         <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen name={'Home'} component={Home} />
           <Stack.Screen name={'EpisodeDetail'} component={EpisodeDetail} />
+          <Stack.Screen name={'Characters'} component={Characters} />
+          <Stack.Screen name={'CharacterDetail'} component={CharacterDetail} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
