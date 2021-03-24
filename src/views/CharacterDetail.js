cf7cb15bd468cce3f58ed86384a81f5c243981cf
yasindemir94 <yasin.demir94@outlook.com>
2021-03-24 16:17:31 +0300

    character details

---
+++ src/views/CharacterDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CharacterDetail = () => {
+    return (
+        <View>
+            <Text>selammmm</Text>
+        </View>
+    )
+}
+
+export default CharacterDetail
